05216a8cf885107249057f428b020eebf87f62fe
drood87 <dbernstein1987@hotmail.com>
2019-06-06 17:20:19 +0800

    check resize of screen

--- resources/js/mobile-nav.js
+++ resources/js/mobile-nav.js
@@ -1,6 +1,5 @@
 const hamburgerButton = document.querySelector('#js-hook-hamburger');
 const mainNav = document.querySelector('#js-hook-main-nav');
-// const logoBlack = document.querySelector('#logo-black');
 
 function expandNav() {
   if (mainNav.style.display === 'block') {
@@ -12,4 +11,17 @@ function expandNav() {
   }
 }
 
+// CHECK IF SCREEN WIDTH
+
+function checkScreenWidth() {
+  const viewportWidth = window.innerWidth;
+  if (viewportWidth > 800) {
+    mainNav.style.display = 'flex';
+  } else {
+    mainNav.style.display = 'none';
+    // expandNav();
+  }
+}
+
 hamburgerButton.addEventListener('click', expandNav);
+window.addEventListener('resize', checkScreenWidth);
